17b247b2a8f91b6c78b21c71139cad408b015600
RatRatARMY <156580271+kalksdlkdsa@users.noreply.github.com>
2024-02-09 20:19:33 +0700

    Some another

--- main.ts
+++ main.ts
@@ -1,21 +1,18 @@
-function main() {
-    
+function main () {
     pass2 = 1
     basic.showNumber(pass2)
     serial.writeNumber(pass2)
     pass2 = 2
     basic.showNumber(pass2)
     serial.writeNumber(pass2)
-    for (let i = 0; i < 25000; i++) {
+    for (let i = 0; i <= 24999; i++) {
         if (i <= 1000) {
             basic.showNumber(i)
             serial.writeNumber(i)
         } else {
-            break
+            break;
         }
-        
     }
 }
-
 let pass2 = 0
 main()
