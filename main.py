5991e361c0f78fbba4ea212fabace9ff4c7452d1
RatRatARMY <156580271+kalksdlkdsa@users.noreply.github.com>
2024-02-09 17:59:15 +0700

    Some changes for my program

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-# The function main() to run program
-def main():
-    basic.show_string("Hello world!")
-    basic.show_string("Today, I will show you something")
-    basic.show_number(12003)
-    basic.show_string("Can you see this number?")
-    serial.write_line("This is the int")
-    serial.write_line("int32")
-    basic.show_string("int32")
-    serial.write_line("int16")
-    basic.show_string("int16")
-    serial.write_line("int8")
-    basic.show_string("int8")
-    serial.write_line("int64")
-    basic.show_string("int64")
-    serial.write_line("int128")
-    basic.show_string("int128")
-# Call the main() function
-main()
+basic.show_leds("""
+# . # . #
+. # . # .
+# # # # #
+. . . . .
+# . # . #
+""")
