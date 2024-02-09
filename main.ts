de9ccdcbd2586b4bee138b3d97716aa1ff7d6c9d
RatRatARMY <156580271+kalksdlkdsa@users.noreply.github.com>
2024-02-09 17:55:43 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+//  The function main() to run program
+function main() {
+    basic.showString("Hello world!")
+    basic.showString("Today, I will show you something")
+    basic.showNumber(12003)
+    basic.showString("Can you see this number?")
+    serial.writeLine("This is the int")
+    serial.writeLine("int32")
+    basic.showString("int32")
+    serial.writeLine("int16")
+    basic.showString("int16")
+    serial.writeLine("int8")
+    basic.showString("int8")
+    serial.writeLine("int64")
+    basic.showString("int64")
+    serial.writeLine("int128")
+    basic.showString("int128")
+}
+
+//  Call the main() function
+main()
