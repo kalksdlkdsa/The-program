5445b4a111e67e9b261169e8993319b0adf0b83b
RatRatARMY <156580271+kalksdlkdsa@users.noreply.github.com>
2024-02-09 18:10:54 +0700

    Some change (again)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-basic.show_leds("""
-# . # . #
-. # . # .
-# # # # #
-. . . . .
-# . # . #
-""")
+def ox_oy_oxy(x:int):
+    show_number(x)
+def on_button_pressed_a():
+    ox_oy_oxy(87928)
+    ox_oy_oxy(18)
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
-basic.showLeds(`
-# . # . #
-. # . # .
-# # # # #
-. . . . .
-# . # . #
-`)
+function main () {
+    pass = 1
+    basic.showNumber(pass)
+    serial.writeNumber(pass)
+    pass = 2
+    basic.showNumber(pass)
+    serial.writeNumber(pass)
+    for (let i = 0; i <= 24999; i++) {
+        if (i <= 1000) {
+            basic.showNumber(i)
+        } else {
+            break;
+        }
+    }
+}
+let pass = 0
+main()
